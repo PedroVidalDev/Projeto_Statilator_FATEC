932fc9dd93088857454135fe013be4dd7b2ad00c
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-01 09:49:21 -0300

    [feat] - criando funcao de analise quantitativa, por cada intervalo

--- application/analiseDadoQnt.py
+++ application/analiseDadoQnt.py
@@ -10,7 +10,8 @@ def analiseQntFront(user):
     def sairFunction():
         app.destroy()
 
-    casos = DadoService.analiseQnt(user)
+    analise = DadoService.analiseQnt(user)
+    dadosTabela = DadoService.organizarDadosTabelaQnt(user, analise[8], analise[9])
     
     customtkinter.set_appearance_mode("dark")
     customtkinter.set_default_color_theme("blue")
@@ -23,29 +24,56 @@ def analiseQntFront(user):
     labelTitle = customtkinter.CTkLabel(master=app, text="Statilator", font=customtkinter.CTkFont(size=60, weight="bold"))
     labelTitle.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
 
-    labelMaior = customtkinter.CTkLabel(master=app, text=f"Maior numero = {casos[0]}", font=customtkinter.CTkFont(size=15, weight="bold"))
+    labelMaior = customtkinter.CTkLabel(master=app, text=f"Maior numero = {analise[0]}", font=customtkinter.CTkFont(size=15, weight="bold"))
     labelMaior.place(relx=0.25, rely=0.2, anchor=customtkinter.CENTER)
 
-    labelMenor = customtkinter.CTkLabel(master=app, text=f"Menor numero = {casos[1]}", font=customtkinter.CTkFont(size=15, weight="bold"))
+    labelMenor = customtkinter.CTkLabel(master=app, text=f"Menor numero = {analise[1]}", font=customtkinter.CTkFont(size=15, weight="bold"))
     labelMenor.place(relx=0.5, rely=0.2, anchor=customtkinter.CENTER)
 
-    labelAmplitude = customtkinter.CTkLabel(master=app, text=f"Amplitude = {casos[2]}", font=customtkinter.CTkFont(size=15, weight="bold"))
+    labelAmplitude = customtkinter.CTkLabel(master=app, text=f"Amplitude = {analise[2]}", font=customtkinter.CTkFont(size=15, weight="bold"))
     labelAmplitude.place(relx=0.75, rely=0.2, anchor=customtkinter.CENTER)
 
-    labelMedia = customtkinter.CTkLabel(master=app, text=f"Media = {casos[3]}", font=customtkinter.CTkFont(size=15, weight="bold"))
+    labelMedia = customtkinter.CTkLabel(master=app, text=f"Media = {analise[3]}", font=customtkinter.CTkFont(size=15, weight="bold"))
     labelMedia.place(relx=0.25, rely=0.25, anchor=customtkinter.CENTER)
 
-    labelMediana = customtkinter.CTkLabel(master=app, text=f"Mediana = {casos[4]}", font=customtkinter.CTkFont(size=15, weight="bold"))
+    labelMediana = customtkinter.CTkLabel(master=app, text=f"Mediana = {analise[4]}", font=customtkinter.CTkFont(size=15, weight="bold"))
     labelMediana.place(relx=0.5, rely=0.25, anchor=customtkinter.CENTER)
 
-    labelModa = customtkinter.CTkLabel(master=app, text=f"Mediana = {casos[5]}", font=customtkinter.CTkFont(size=15, weight="bold"))
+    labelModa = customtkinter.CTkLabel(master=app, text=f"Mediana = {analise[5]}", font=customtkinter.CTkFont(size=15, weight="bold"))
     labelModa.place(relx=0.75, rely=0.25, anchor=customtkinter.CENTER)
 
-    labelPrimeiroQuartil = customtkinter.CTkLabel(master=app, text=f"1 quartil = {casos[6]}", font=customtkinter.CTkFont(size=15, weight="bold"))
+    labelPrimeiroQuartil = customtkinter.CTkLabel(master=app, text=f"1 quartil = {analise[6]}", font=customtkinter.CTkFont(size=15, weight="bold"))
     labelPrimeiroQuartil.place(relx=0.25, rely=0.30, anchor=customtkinter.CENTER)
 
-    labelTerceiroQuartil = customtkinter.CTkLabel(master=app, text=f"3 quartil = {casos[7]}", font=customtkinter.CTkFont(size=15, weight="bold"))
+    labelTerceiroQuartil = customtkinter.CTkLabel(master=app, text=f"3 quartil = {analise[7]}", font=customtkinter.CTkFont(size=15, weight="bold"))
     labelTerceiroQuartil.place(relx=0.5, rely=0.30, anchor=customtkinter.CENTER)
+
+    columns = ("Intervalo", "Qnt.", "Fr", "Fr%", "Fr Acu.")
+
+    style = ttk.Style()
+    style.configure("Treeview",
+                    background="grey",
+                    foreground="lightgreen"
+                    )
+
+    tv = ttk.Treeview(master=app, column=columns, show="headings")
+
+    tv.column("Intervalo", anchor=CENTER)
+    tv.column("Qnt.", anchor=CENTER)
+    tv.column("Fr", anchor=CENTER)
+    tv.column("Fr%", anchor=CENTER)
+    tv.column("Fr Acu.", anchor=CENTER)
+
+    tv.heading("Intervalo", text="Intervalo", anchor=CENTER)
+    tv.heading("Qnt.", text="Qnt.", anchor=CENTER)
+    tv.heading("Fr", text="Fr", anchor=CENTER)
+    tv.heading("Fr%", text="Fr%", anchor=CENTER)
+    tv.heading("Fr Acu.", text="Fr Acu.", anchor=CENTER)
+
+    tv.place(relx=0.5, rely=0.2, anchor=customtkinter.CENTER)
+
+    for dado in dadosTabela:
+        tv.insert("",END, values=dado)
 
     buttonSairMenu = customtkinter.CTkButton(master=app, text="Sair", command=lambda:sairFunction())
     buttonSairMenu.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
--- domain/DadoService.py
+++ domain/DadoService.py
@@ -10,9 +10,6 @@ def registrarQnt(user, dado):
     registrar = dadoDAO.registrarDadoQnt(user, dado)
     return registrar
 
-def mostrar(user):
-    return dadoDAO.read(user)
-
 def analisePareto(user):
     dados = dadoDAO.readPareto(user)
     dados_sem_duplicata = list(set(dados))
@@ -68,7 +65,91 @@ def analiseQnt(user):
     mediana = np.quantile(listaDados, .5)
     terceiroQuartil = np.quantile(listaDados, .75)
 
-    return [maiorNum, menorNum, amplitude, media, mediana, moda, primeiroQuartil, terceiroQuartil]
+    qntClasses = 1 + 3.33 * np.log10(len(listaDados))
+    tamanhoClasse = round(amplitude / qntClasses)
+
+    return [maiorNum, menorNum, amplitude, media, mediana, moda, primeiroQuartil, terceiroQuartil, qntClasses, tamanhoClasse]
+
+def organizarDadosTabelaQnt(user, qntClasse, tamanhoClasse):
+    dados = dadoDAO.readQnt(user)
+    listaDados = []
+
+    for i in range(len(dados)):
+        listaDados.append(dados[i][0])
+
+    listaDados.sort()
+
+    # OBTENDO OS INTERVALOS DA TABELA PARA ANALISE #
+    intervalos = [listaDados[0]]
+    intervaloAtual = intervalos[0]
+    
+    while intervaloAtual <= listaDados[len(listaDados) - 1]:
+        intervaloAtual = intervaloAtual + tamanhoClasse
+        intervalos.append(intervaloAtual)
+
+    # OBTENDO QNT DE OCORRENCIA EM CADA INTERVALO #
+    qntDeOcorrencia = []
+    contador = 0
+
+    for i in range(len(intervalos) - 1):
+
+        for j in range(len(listaDados)):
+            if(listaDados[j] >= intervalos[i] and listaDados[j] < intervalos[i+1]):
+                contador = contador + 1
+
+        qntDeOcorrencia.append(contador)
+        contador = 0
+
+    # OBTENDO A FREQUENCIA DE CADA INTERVALO TENDO COMO BASE O TOTAL DE OCORRENCIAS #
+    totalOcorrencia = len(listaDados)
+    listaFr = []
+
+    for i in range(len(qntDeOcorrencia)):
+        freq = qntDeOcorrencia[i] / totalOcorrencia
+        listaFr.append(freq)
+
+    # OBTENDO A FREQUENCIA EM PORCENTAGEM #
+    listaFrPorc = []
+
+    for i in range(len(listaFr)):
+        listaFrPorc.append(listaFr[i] * 100)
+
+    # OBTENDO A FRENQUENCIA ACUMULADA #
+    listaFrAcumulada = [0]
+    contadorFr = 0
+
+    for i in range(len(listaFrPorc)):
+        contadorFr = contadorFr + listaFrPorc[i]
+        listaFrAcumulada.append(contadorFr)
+
+    print(listaFrAcumulada)
+
+    # FORMATANDO FORMA QUE OS INTERVALOS SERAO MOSTRADOS NA TABELA #
+    intervalosTexto = []
+
+    for i in range(len(intervalos) - 1):
+        texto = f"{intervalos[i]}| --{intervalos[i+1]}"
+        intervalosTexto.append(texto)
+
+    # COLOCANDO TUDO DENTRO DE APENAS UMA LISTA #
+    dadosTabela = []
+    for i in range(len(intervalosTexto)):
+        dadosTabela.append([
+            intervalosTexto[i],
+            qntDeOcorrencia[i],
+            listaFr[i],
+            listaFrPorc[i],
+            listaFrAcumulada[i]
+        ])
+    dadosTabela.append([
+        "Total",
+        sum(qntDeOcorrencia),
+        sum(listaFr),
+        sum(listaFrPorc),
+        listaFrAcumulada[len(listaFrAcumulada) - 1],
+    ])
+
+    return dadosTabela
 
 def deletar(user):
     return dadoDAO.deletar(user)
